bbbfacd3b188a93c19ab202eb11b4c040cf81f0b
Mihai C <mciorobe@adobe.com>
2015-02-04 21:10:26 +0200

    Added workspace UI indicator

--- js/popup.js
+++ js/popup.js
@@ -3,21 +3,49 @@ var workspaceApp = angular.module('workspaceApp', []);
 function workspaceAppController($scope, $http) {
 
     $scope.automaticallyUpdate = true;
+    $scope.newWorkspaceName = "";
 
-    $scope.getAllworkspace = function () {
-        return [
-            {"name": "1"},
-            {"name": "2"}
-        ];
+    //
+    $scope.workspaces = [];
+    $scope.currentWorkspacesIndex = -1;
+
+    $scope.getAllWorkspace = function () {
+        return $scope.workspaces;
     };
 
-    $scope.docs = $scope.getAllworkspace();
+    $scope.docs = $scope.getAllWorkspace();
 
     // Check the number of saved workspaces
-    $scope.hasworkspaces = function () {
-        var allworkspace = $scope.getAllworkspace();
-        return angular.isDefined(allworkspace ) && allworkspace.length != 0;
+    $scope.hasWorkspaces = function () {
+        var allWorkspace = $scope.getAllWorkspace();
+        return angular.isDefined(allWorkspace) && allWorkspace.length != 0;
     };
+
+    // Create new workspace ==> create a new empty window
+    $scope.createNewWorkspace = function (name) {
+        $scope.workspaces.push({"name": name});
+    }
+
+    // Save new workspace from the current windows
+    $scope.saveWorkspace = function (name) {
+        var newWorkspace = {
+            "name": name,
+            "windows": []
+        };
+
+        chrome.windows.getAll({"populate": true}, function (windows) {
+            windows.forEach(function (window) {
+                var windowTabs = [];
+                window.tabs.forEach(function (tab) {
+                    windowTabs.push(tab.url);
+                });
+                newWorkspace.windows.push(windowTabs)
+            })
+        });
+
+        $scope.workspaces.push(newWorkspace);
+        $scope.currentWorkspacesIndex = 0;
+    }
 
 }
 
